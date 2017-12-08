f37dee8ba2cf0c4156d55489578e06b037367522
Guest User <Guest@Epicodus-8A.local>
2017-12-08 11:10:13 -0800

    add first spec: return count

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
 // Business Logic
-var special = function (userInput) {
+var special = function(userInput) {
+  debugger;
+  var numbers = [];
+  for (index = 0; index < userInput + 1; index+= 1) {
+    numbers.push(index);
+    alert(numbers)
+  }
+  if (userInput === 0) {
+    alert ("beep!")
+  }
+  else if (userInput ===1) {
+    alert("boop!")
+  }
+
+
   // var parsedNumber = parseInt(numbers);
-  if ((userInput) === 1) {
-    return false;
-    ;
+  // if ((userInput) === 1) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
     // console.log(numbers)
   };
-}
+
 
 
 
@@ -25,12 +41,18 @@ $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     var userInput = parseInt($("input").val());
-    var result = special (userInput);
+    special(userInput);
+
+    // $("numberInput").hide();
+
+
     // special (numbers);
   // console.log(numbers);
   // $("#result").text(parsedNumber);
-  if (!result) {
-    $("#result").text(userInput);
-  }
+  // $("#result").text("beep!");
+  //
+  // if (!result) {
+  //   $("#result").append(numbers[index]);
   })
+
 });
