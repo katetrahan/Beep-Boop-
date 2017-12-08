8a79cf9ccbd56f8a96bd531396556a6bd6c64f9a
Guest User <Guest@Epicodus-8A.local>
2017-12-08 14:15:18 -0800

    add spec two

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,43 @@ var special = function(number) {
   debugger;
   var tmp = number;
   var result = [];
-  if (tmp === 1) {
-    return "boop";
-  }
-  else if (tmp === 0) {
-    return "beep";
-  }
-  else if (tmp !== 1 && tmp !== 0) {
-    for (var index = 0; index < number + 1; index+= 1) {
-      result.push(index);
-  }
-    // alert(numbers)
-  }
-  return result
-  // return numbers
-  // if (number === 0) {
-  //   alert("beep!")
+  // var split = tmp.split(""); // added
+  var exceptions = ["0","1"]; // added
+  // for (var indexOne = 0; indexOne < split.length; index +=1) { // added
+  //   for (var i = 0; i < exceptions.length; i += 1) { // added
+  //     if (split[indexOne] === exceptions[i]) {
+  //       split.splice(indexOne,1,"beep");
+  //     }
+  //   }
   // }
-  // else if (number ===1) {
-  //   alert("boop!")
+  for (var i = 0; i < result.length; i += 1) {
+    result.push(i);
+    for (var index = 0; index < exceptions.length; index += 1) {
+      if (result === exceptions){
+        return "beep"
+      }
+    }
   }
 
-
-  // var parsedNumber = parseInt(numbers);
-  // if ((userInput) === 1) {
-  //   return true;
-  // } else {
-  //   return false;
+  // below works
+  //  if (tmp === 1) {
+  //    return "boop";
+  //  }
+  // else if (tmp === 0) {
+  //   return "beep";
   // }
-    // console.log(numbers)
+  //  else if (tmp !== 1 && tmp !== 0) {
+  //    for (var index = 0; index < number + 1; index+= 1) {
+  //      result.push(index);
+   }
+     // alert(numbers)
+  // }
+  // return result
+
+// };
+
+
+
 
 
 
